6535de8b1352d432a9f8ca9333588cfe07885457
Unknown <dani_mrx93@hotmail.com>
2019-03-10 18:25:31 -0300

    Add DoubleDamage

--- Object/Proyectile.gd
+++ Object/Proyectile.gd
@@ -6,6 +6,7 @@ var can_damage = true
 var tank_damage
 var tank_owner
 var direction = Vector2()
+var double_damage
 
 signal hit(distance)
 
@@ -17,11 +18,20 @@ func _process(delta):
 	translate(direction * speed * delta)
 	pass
 
-func initialize(dir, damage, player):
+func initialize(dir, damage, _double_damage, player):
 	direction = dir
 	rotation = direction.angle()
 	tank_damage = damage
 	tank_owner = player
+	double_damage = _double_damage
+	$DoubleDamage.emitting = double_damage
+	if double_damage:
+		$Trail.amount *= 3
+		$Explosion.amount *= 3
+		$Explosion.process_material.initial_velocity = 60
+	else:
+		$Explosion.process_material.initial_velocity = 50
+	pass
 
 func _on_Proyectile_body_entered(body):
 	if body.is_in_group('Tank') and body != tank_owner and body.is_alive() and can_damage:
@@ -33,9 +43,13 @@ func _on_Proyectile_body_entered(body):
 
 func hit_something():
 	can_damage = !can_damage
-	emit_signal('hit',global_position.distance_to(tank_owner.global_position))
+	var shake_additive = 1
+	if double_damage:
+		shake_additive *= 2
+	emit_signal('hit',global_position.distance_to(tank_owner.global_position) / shake_additive)
 	$Explosion.emitting = true
 	$Trail.emitting = false
+	$DoubleDamage.emitting = false
 	set_process(false)
 	$Anim.play('Explosion')
 	$LifetimeTimer.start()
